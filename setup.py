affdf44e1476e530850f4c7c43c4f8318353fc1f
Abhiram Pola <polaabhiram63@gmail.com>
2026-01-26 15:04:54 +0530

    Created basic setup file to package the application

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="ML Project",
+    version="0.0.1",
+    description="This is a basic implementation of predicting student performance",
+    author="Abhiram",
+    author_email="polaabhiram63@gmail.com",
+    packages=find_packages()
+)
